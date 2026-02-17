91a034fa0e079d0dcd0d4e128ed9e34bf7f25c03
Igor Shmukler <igor.shmukler@gmail.com>
2026-02-17 09:45:00 -0500

    bumped version number to corelate bug reports with changes

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ if readme_file.exists():
 
 setup(
     name="kokoro-ruslan",
-    version="0.0.5",
+    version="0.0.6",
     author="Igor Shmukler",
     description="Transformer-based Russian Text-to-Speech System",
     long_description=long_description,
